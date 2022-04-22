5b9a459368976578af65feedd2e683188ca27872
Nicolas Penailillo <nico.penailillo@gmail.com>
2022-04-22 19:14:26 +0200

    add camera thread, frame grabbing and finger detector in main

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("helo")
+from cam import WebcamVideoStream
+from handDetector import handsDetector
+import cv2
+
+if __name__ == "__main__" :
+    vs = WebcamVideoStream().start()
+    hands = handsDetector()
+
+    while True :
+        try:
+            frame = vs.read()
+            #process by CV algorithm, GUI
+            hands.getLandMarks(frame)
+            cv2.imshow('webcam', frame)
+            if cv2.waitKey(1) == 27 :
+                break
+        except:
+            vs.stop()
+            cv2.destroyAllWindows()
+
+    vs.stop()
+    cv2.destroyAllWindows()
