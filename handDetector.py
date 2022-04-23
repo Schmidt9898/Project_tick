14db61d800348f25c1464f6ec8154be023a346a2
Nicolas Penailillo <nico.penailillo@gmail.com>
2022-04-23 23:52:21 +0200

    add state and position in handDetector

--- handDetector.py
+++ handDetector.py
@@ -1,5 +1,10 @@
+
 import cv2
 import mediapipe as mp
+import numpy as np
+import math
+from itertools import permutations
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -12,18 +17,48 @@ class handsDetector:
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5)
 
-    def getLandMarks(self, frame):
+        self.stateCount = 0
+        self.state = "Unknown"
+        self.height = 0
+        self.width = 0
+        self.cursorPosition = 0
+
+    def getHandAction(self, frame):
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = self.hands.process(image)
-
         # Draw the hand annotations on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_world_landmarks:
+          for hand_landmarks in results.multi_hand_world_landmarks:
+                tTip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                tTip = [tTip.x, tTip.y]
+                iTip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                iTip = [iTip.x, iTip.y]
+
+                dist = math.dist(tTip, iTip)
+                self.height, self.width = image.shape[:2]
+
+                if(dist< 0.05):
+                    if self.state != "Closed":
+                        self.stateCount += 1
+
+                        if self.stateCount == 20:
+                             self.state = "Closed"
+                             self.stateCount = 0
+                else:
+                    if self.state != "Open":
+                        self.stateCount += 1
+
+                        if self.stateCount == 20:
+                             self.state = "Open"
+                             self.stateCount = 0
+
         if results.multi_hand_landmarks:
-          for hand_landmarks in results.multi_hand_landmarks:
-                print("THUMB_TIP",hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP])
-                print("INDEX_FINGER_TIP",hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP])
-                print("MIDDLE_FINGER_TIP",hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP])
-                print("RING_FINGER_TIP",hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP])
-                print("PINKY_TIP",hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP])
+            for hand_landmarks in results.multi_hand_landmarks:
+                tTip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+                tTip = [tTip.x*self.width, tTip.y*self.height]
+
+                self.cursorPosition = tTip
+
--- main.py
+++ main.py
@@ -9,11 +9,13 @@ if __name__ == "__main__" :
     while True :
         try:
             frame = vs.read()
+            frame=cv2.flip(frame,1) # mirror the image
             #process by CV algorithm, GUI
-            hands.getLandMarks(frame)
+            hands.getHandAction(frame)
             cv2.imshow('webcam', frame)
             if cv2.waitKey(1) == 27 :
                 break
+
         except:
             vs.stop()
             cv2.destroyAllWindows()
